b222f9389a11574a52f052cfdd125de2d33c4a38
Mightly Dragon <jingjie105@gmail.com>
2022-09-04 23:59:16 +0800

    Added styling for darkmode toggle switch. Added functionality to darkmode.

--- darkmode.js
+++ darkmode.js
@@ -3,14 +3,12 @@ let darkMode = localStorage.getItem('darkMode');
 const darkModeToggle = document.querySelector('#dark-mode-toggle');
 
 let site_title = document.getElementById("site-title");
-let page_link = document.getElementById("page-link");
 let site_nav = document.getElementById("site-nav");
 
 const enableDarkMode = () => {
   // 1. Add the class to the body
   document.body.classList.add('darkmode');
   site_title?.classList.add("headerStyling");
-  page_link?.classList.add("headerStyling");
   site_nav?.classList.add("navStyling");
   // 2. Update darkMode in localStorage
   localStorage.setItem('darkMode', 'enabled');
@@ -20,7 +18,6 @@ const disableDarkMode = () => {
   // 1. Remove the class from the body
   document.body.classList.remove('darkmode');
   site_title?.classList.remove("headerStyling");
-  page_link?.classList.remove("headerStyling");
   site_nav?.classList.remove("navStyling");
   // 2. Update darkMode in localStorage 
   localStorage.setItem('darkMode', null);
@@ -30,16 +27,25 @@ const disableDarkMode = () => {
 // start things off with it on
 if (darkMode === 'enabled') {
   enableDarkMode();
-}
+
+  //Bring moon infront and hide the sun
+  document.querySelector(".moon-logo").classList.toggle("animate-moon");
+  document.querySelector(".sun-logo").classList.toggle("animate-sun");
+};
 
 // When someone clicks the button
 darkModeToggle.addEventListener('click', () => {
   // get their darkMode setting
   darkMode = localStorage.getItem('darkMode'); 
-  console.log("Test")
+
+  //Animate darkmode btn
+  document.querySelector(".sun-logo").classList.toggle("animate-sun");
+  document.querySelector(".moon-logo").classList.toggle("animate-moon");
   // if it not current enabled, enable it
   if (darkMode !== 'enabled') {
     enableDarkMode();
+
+   
   // if it has been enabled, turn it off  
   } else {  
     disableDarkMode(); 
