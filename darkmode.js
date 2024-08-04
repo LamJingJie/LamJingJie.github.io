5183b12f9dc401366388ca4f69dfc571ddbbddbd
jingjie <jingjie105@gmail.com>
2024-08-04 22:29:45 +0800

    dark mode update

--- darkmode.js
+++ darkmode.js
@@ -1,38 +1,35 @@
 //------------This section is used for website or ios only----------------
 
-const darkModeToggle = document?.querySelector('#dark-mode-toggle');
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
 
-let toggle_container = document?.getElementById("togglecontainer");
+let toggle_container = document.getElementById("togglecontainer");
 let check = false;
 
 // Assume dark mode is off if not set in local storage
 const toggleDarkMode = (status) => {
-  let site_title = document?.getElementById("site-title");
-  let site_nav = document?.getElementById("site-nav");
-  let pfp = document?.getElementById("mypfp");
+  let site_title = document.getElementById("site-title");
+  let site_nav = document.getElementById("site-nav");
   localStorage.setItem('darkMode', status);
   
   if(status){
     // Dark mode is enabled
-    document.body?.classList.add('darkmode');
+    document.body.classList.add('darkmode');
     site_title?.classList.add("headerStyling");
     site_nav?.classList.add("navStyling");
-    pfp?.classList.add("borderStyling");
     console.log("Dark mode enabled");
 
     //Bring moon infront and hide the sun
-    document?.querySelector(".moon-logo").classList.add("animate-moon");
-    document?.querySelector(".sun-logo").classList.add("animate-sun");
+    document.querySelector(".moon-logo").classList.add("animate-moon");
+    document.querySelector(".sun-logo").classList.add("animate-sun");
   }else{
     // Dark mode is disabled
-    document.body?.classList.remove('darkmode');
+    document.body.classList.remove('darkmode');
     site_title?.classList.remove("headerStyling");
     site_nav?.classList.remove("navStyling");
-    pfp?.classList.remove("borderStyling");
 
     // Bring sun infront and hide the moon
-    document?.querySelector(".sun-logo").classList.remove("animate-sun");
-    document?.querySelector(".moon-logo").classList.remove("animate-moon");
+    document.querySelector(".sun-logo").classList.remove("animate-sun");
+    document.querySelector(".moon-logo").classList.remove("animate-moon");
   }
 
 }
@@ -70,7 +67,6 @@ let matched = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
 //if true means user is on android
 if (check) {
-
   this.checkColorScheme();
 
   //remove btn to toggle dark mode
@@ -82,7 +78,6 @@ if (check) {
 
   //If the user already visited and enabled darkMode
   if (darkMode === 'enabled') {
-    console.log("Dark mode enabled2");
     toggleDarkMode('enabled');
   };
 }
@@ -93,17 +88,14 @@ if (check) {
 function checkColorScheme() {
   let site_title = document.getElementById("site-title");
   let site_nav = document.getElementById("site-nav");
-  let pfp = document.getElementById("mypfp");
 
   if (matched) {
     document.body.classList.add('darkmode');
     site_title?.classList.add("headerStyling");
     site_nav?.classList.add("navStyling");
-    pfp?.classList.add("borderStyling");
   } else {
     document.body.classList.remove('darkmode');
     site_title?.classList.remove("headerStyling");
     site_nav?.classList.remove("navStyling");
-    pfp?.classList.remove("borderStyling");
   }
 }
